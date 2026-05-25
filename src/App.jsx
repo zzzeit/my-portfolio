07ec552be0959215ccd3c13baf09b605f5a8ee0e
Nel <neilzeith@gmail.com>
2026-05-25 19:16:18 +0800

    Projects Component

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
 import Home from './components/Home'
+import Projects from './components/Projects'
 
 function App() {
 
 	return (
 		<>	
 			<Home />
+			<Projects />
 		</>
 	)
 }
---
+++ src/components/Projects.jsx
@@ -0,0 +1,13 @@
+import Section from './Section'
+
+function Projects() {
+    return (
+        <Section id="projects">
+            <div>
+                <h1>Projects</h1>
+            </div>
+        </Section>
+    )
+}
+
+export default Projects
