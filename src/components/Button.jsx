162c30530def9f114e56952e238b16833a7046b1
Nel <neilzeith@gmail.com>
2026-05-25 18:55:56 +0800

    updated Button component
    can be configured to be header type button or regular button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,9 +1,17 @@
 import './Button.css'
 
-function Button({ Text, activeNumber, onClick }) {
+function Button({ text, activeNumber, onClick , style}) {
+    if (!activeNumber) {
+        return (
+            <button className="button-goto-projects" onClick={onClick} style={style}>
+                {text}
+            </button>
+        )
+    }
+
     return (
         <button className={`my-button ${activeNumber[0] === activeNumber[1] ? 'active' : ''}`} onClick={onClick}>
-            {Text}
+            {text}
         </button>
     )
 }
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -9,9 +9,9 @@ function Header() {
         <>
             <div className="my-header">
                 <div className="buttons-container">
-                    <Button Text="Home" activeNumber={[activeHeaderButton, 1]} onClick={() => setActiveHeaderButton(1)} />
-                    <Button Text="Projects" activeNumber={[activeHeaderButton, 2]} onClick={() => setActiveHeaderButton(2)} />
-                    <Button Text="Contact" activeNumber={[activeHeaderButton, 3]} onClick={() => setActiveHeaderButton(3)} />
+                    <Button text="Home" activeNumber={[activeHeaderButton, 1]} onClick={() => setActiveHeaderButton(1)} />
+                    <Button text="Projects" activeNumber={[activeHeaderButton, 2]} onClick={() => setActiveHeaderButton(2)} />
+                    <Button text="Contact" activeNumber={[activeHeaderButton, 3]} onClick={() => setActiveHeaderButton(3)} />
                 </div>
             </div>
 
