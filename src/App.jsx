ac76aa4b4c6e8f723afeed4a8ff58591e5d86703
Nel <neilzeith@gmail.com>
2026-05-25 09:47:10 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from './assets/vite.svg'
+import heroImg from './assets/hero.png'
+import './App.css'
+
+function App() {
+
+	return (
+		<>
+			
+		</>
+	)
+}
+
+export default App
