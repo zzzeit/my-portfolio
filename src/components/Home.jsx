f0ac1ae830eed802dfd6a56c934a6c14d91f99be
Nel <neilzeith@gmail.com>
2026-05-26 21:52:39 +0800

    Update Home.jsx

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,11 +3,16 @@ import Section from './Section'
 import ProfileIMG from '../assets/profile.jpg'
 import Button from './Button'
 
-function scrollToProjects() {
-    document.getElementById('projects')?.scrollIntoView({ behavior: 'smooth' })
-}
-
 function Home() {
+
+    const scrollToProjects = () => {
+        document.getElementById('projects')?.scrollIntoView({ behavior: 'smooth' })
+    }
+
+    const scrollToContact = () => {
+        document.getElementById('contact')?.scrollIntoView({ behavior: 'smooth' })
+    }
+
     return (
         <Section>
             <div className="my-home">
@@ -17,7 +22,7 @@ function Home() {
                     <p>I'm a software developer specializing in building exceptional digital experiences. Currently, I'm focused on building responsive web applications.</p>
                     <div className="home-buttons">
                         <Button text="View My Projects" onClick={scrollToProjects} />
-                        <Button text="Lets Connect" style={{ backgroundColor: '#252525' }} />
+                        <Button text="Lets Connect" style={{ backgroundColor: '#252525' }} onClick={scrollToContact} />
                     </div>
                     
                 </div>
