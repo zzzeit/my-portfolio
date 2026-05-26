efdaaac5116e7457d787c5a11a024167d618928f
Nel <neilzeith@gmail.com>
2026-05-26 21:21:22 +0800

    Contact Section update

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,6 +1,7 @@
 import './Contact.css'
 import Section from './Section'
 import { useState } from 'react'
+import Notification from './Notification'
 
 function Contact() {
 
@@ -9,6 +10,7 @@ function Contact() {
     const [message, setMessage] = useState('')
 
     const handleSubmit = (e) => {
+        e.preventDefault()
         if (!name || !email || !message) {
             alert('Please fill in all fields before submitting.')
             return
@@ -21,15 +23,21 @@ function Contact() {
                 <h1>Get in Touch</h1>
                 <div className="decorator-divider"/>
                 <h3 style={{ marginTop: '0' }}>Have a question or want to interview me? Drop me a line!</h3>
-                <div className="form-contact">
-                    <label htmlFor="name">Name</label>
-                    <input type="text" id="name" name="name" placeholder="Your Name" value={name} onChange={(e) => setName(e.target.value)} />
-                    <label htmlFor="email">Email</label>
-                    <input type="email" id="email" name="email" placeholder="you@example.com" value={email} onChange={(e) => setEmail(e.target.value)} />
-                    <label htmlFor="message">Message</label>
-                    <textarea id="message" name="message" placeholder="Write something..." style={{ height: '200px' }} value={message} onChange={(e) => setMessage(e.target.value)}></textarea>
-                    <input type="submit" value="Submit" onClick={handleSubmit}/>
-                </div>
+                <form className="form-contact" onSubmit={handleSubmit}>
+                    <div className="form-group">
+                        <label htmlFor="name">Name</label>
+                        <input type="text" id="name" name="name" placeholder="Your Name" value={name} onChange={(e) => setName(e.target.value)} />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="email">Email</label>
+                        <input type="email" id="email" name="email" placeholder="you@example.com" value={email} onChange={(e) => setEmail(e.target.value)} />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="message">Message</label>
+                        <textarea id="message" name="message" placeholder="Write something..." style={{ height: '200px' }} value={message} onChange={(e) => setMessage(e.target.value)}></textarea>
+                    </div>
+                    <input type="submit" value="Submit" />
+                </form>
             </div>
         </Section>
     )
