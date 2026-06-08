c8d5691d35c5cd1bcd092a983bbfe23e4a5dd22a
Nel <neilzeith@gmail.com>
2026-06-08 18:52:48 +0800

    Refactor Repositories CSS and layout

--- src/components/Repositories.jsx
+++ src/components/Repositories.jsx
@@ -55,17 +55,17 @@ function Repositories() {
 
     return (
         <Section id="repositories">
-            <div className="my-repositories h-[1200px] lg:h-[calc(100vh-73px)] w-full pl-5 pr-5 pt-4 pb-4 lg:pl-20 lg:pr-20">
-                <div className="bordered-container h-full  w-full">
+            <div className="my-repositories w-full h-[1200px] pt-4 pr-5 pb-4 pl-5 lg:h-[calc(100vh-73px)] lg:pr-20 lg:pl-20">
+                <div className="bordered-container w-full h-full">
                     <h1 id="repositoriesTitle">My Repositories</h1>
                     <div className="decorator-divider" />
                     <h3 className="mb-7">These are the repositories for both real/mock projects I've created:</h3>
                     
-                    <div className="flex max-[700px]:flex-col min-[700px]:justify-center min-[700px]:max-h-[500px] w-full lg:flex-row gap-10">
+                    <div className="flex w-full gap-10 lg:flex-row max-[700px]:flex-col min-[700px]:justify-center min-[700px]:max-h-[500px]">
 
                         <div className="repositories-list-container w-full lg:max-w-[300px]">
-                            <div className="search-container relative mb-5 w-full lg:mb-0">
-                                <svg className="absolute right-[2%] bottom-[10%]" xmlns="http://www.w3.org/2000/svg" width="2em" height="2em" viewBox="0 0 24 24"><title xmlns="">round-search</title><path fill="currentColor" d="M15.5 14h-.79l-.28-.27a6.5 6.5 0 0 0 1.48-5.34c-.47-2.78-2.79-5-5.59-5.34a6.505 6.505 0 0 0-7.27 7.27c.34 2.8 2.56 5.12 5.34 5.59a6.5 6.5 0 0 0 5.34-1.48l.27.28v.79l4.25 4.25c.41.41 1.08.41 1.49 0s.41-1.08 0-1.49zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5S14 7.01 14 9.5S11.99 14 9.5 14"/></svg>
+                            <div className="search-container relative w-full mb-5 lg:mb-0">
+                                <svg className="absolute bottom-[10%] right-[2%]" xmlns="http://www.w3.org/2000/svg" width="2em" height="2em" viewBox="0 0 24 24"><title xmlns="">round-search</title><path fill="currentColor" d="M15.5 14h-.79l-.28-.27a6.5 6.5 0 0 0 1.48-5.34c-.47-2.78-2.79-5-5.59-5.34a6.505 6.505 0 0 0-7.27 7.27c.34 2.8 2.56 5.12 5.34 5.59a6.5 6.5 0 0 0 5.34-1.48l.27.28v.79l4.25 4.25c.41.41 1.08.41 1.49 0s.41-1.08 0-1.49zm-6 0C7.01 14 5 11.99 5 9.5S7.01 5 9.5 5S14 7.01 14 9.5S11.99 14 9.5 14"/></svg>
                                 <input
                                     type="text"
                                     placeholder="Search repositories..."
@@ -75,7 +75,7 @@ function Repositories() {
                                 />
                             </div>
 
-                            <div className="grid-card flex flex-col h-[300px] min-[700px]:h-[calc(100%-62px)] w-full lg:flex-col gap-4 overflow-y-scroll scrollbar-none">
+                            <div className="grid-card flex flex-col w-full h-[300px] gap-4 overflow-y-scroll scrollbar-none lg:flex-col min-[700px]:h-[calc(100%-62px)]">
                                 {Array.isArray(fetchedRepos.items) && fetchedRepos.items.map((repo) => (
                                     <RepositoryCard key={repo.id} repo={repo} onClick={() => {
                                         setSelectedRepo(repo);
