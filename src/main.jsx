2836e2a1adac8307ec3cf177ef19e11862c76d3f
Nel <neilzeith@gmail.com>
2026-06-10 02:33:33 +0800

    header component fixes

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import Header from './components/Header'
+import Header from './components/Header.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
